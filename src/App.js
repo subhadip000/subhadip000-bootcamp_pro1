961ccf8f16181101ffb6bf3338c26f79e95cecce
Subhadip Mandal <87107246+subhadip000@users.noreply.github.com>
2022-08-18 20:25:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,41 @@
+export default function App() {
+
+
+  return (
+    <div className="App">
+      <table>
+        <tbody>
+          <tr>
+            <td>Name</td>
+            <td>Subhadip</td>
+            <td>Rohan</td>
+            <td>Olivia</td>
+
+          </tr>
+          <tr>
+            <td>Last Name</td>
+            <td>Mandal</td>
+            <td>Manna</td>
+            <td>Dey</td>
+
+          </tr>
+          <tr>
+            <td>Age</td>
+            <td>19</td>
+            <td>30</td>
+            <td>24</td>
+
+          </tr>
+          <tr>
+            <td>Occupation</td>
+            <td>Student</td>
+            <td>IT</td>
+            <td>CEO</td>
+          </tr>
+
+
+        </tbody>
+      </table>
+    </div>
+  );
+}
